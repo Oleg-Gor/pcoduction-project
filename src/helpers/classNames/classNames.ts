3d8777443404a85dbe5fbc0db8d555455031b96a
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-05 19:52:00 +0200

    [12] Add Navbar and create Link as UI component

--- src/helpers/classNames/classNames.ts
+++ src/helpers/classNames/classNames.ts
@@ -1,11 +1,9 @@
-import { access } from "fs"
-
 type Mods = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods).reduce((acc, [className, value]) => value ? [...acc, className] : acc, [])
     ].join(' ')
 }
