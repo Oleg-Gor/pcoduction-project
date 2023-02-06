05a958f3006717336f4dad98e697de237950f15a
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-06 21:43:36 +0200

    [12] Svg loader. File loader. Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,19 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./type/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -32,6 +45,8 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
+        svgLoader,
+        fileLoader,
         typescriptLoader,
         scssLoader]
 }
