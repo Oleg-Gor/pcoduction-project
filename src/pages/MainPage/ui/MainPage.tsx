c5bb6f159cff864a57c10bdd4ed83d95abcd839a
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-12 17:47:10 +0200

    [22] ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,16 @@
+import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
-    return <div className="p">{t('MAIN PAGE')}</div>;
+    return (
+        <>
+            <div>{t('MAIN PAGE')}</div>
+            <BugButton />
+        </>
+
+    );
 };
 
 export default MainPage;
