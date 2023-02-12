eb3248f79dd534ac8f958aeba7f82f253a8a37c0
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-12 17:47:10 +0200

    [22] ErrorBoundary

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'helpers/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 import cls from './Navbar.module.scss';
 
@@ -13,7 +12,7 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
 
     return (
-        <div className={classNames(cls.Navbar, {}, [])}>
+        <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to="./">
                     {t('Main')}
