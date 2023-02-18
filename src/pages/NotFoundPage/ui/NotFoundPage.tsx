3be75fc6705301b325d6efff7c35ca30915edf3b
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-18 19:26:48 +0200

    [27] CI pipeline

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'helpers/classNames/classNames';
 
-import cls from './NotFoundPage.module.scss';
+// import cls from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
     const { t } = useTranslation('notFoundPage');
 
-    return <div className={classNames(cls.NotFoundPage, {}, [])}>{t('NOT FOUND PAGE')}</div>;
+    return <div className={classNames('cls.NotFoundPage', {}, [])}>{t('NOT FOUND PAGE')}</div>;
 };
 
 export default NotFoundPage;
