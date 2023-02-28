9ff7d08ccde7f5ada3c9dcc1ab24a7521a521cbc
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-28 20:12:16 +0200

    [28] Test report

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,13 +1,14 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { componentRender } from 'shared/config/test/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('Sidebar text', () => {
-        render(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Sidebar toggle', () => {
-        render(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleButton = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleButton);
