0efda20292a8727562aca01d8d76624cdc9a75ba
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-02-28 20:12:16 +0200

    [28] Test report

---
+++ src/shared/config/test/componentRender/componentRender.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+export interface ComponentRenderOptions {
+    route?: string;
+}
+
+export function componentRender(component: ReactNode, options:ComponentRenderOptions = {}) {
+    const { route = '/' } = options;
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            {component}
+        </MemoryRouter>,
+    );
+}
