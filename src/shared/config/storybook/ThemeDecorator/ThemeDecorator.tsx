dc0be71fa709b0b24b45dcee165905d7623507e2
Oleg Gorbulich <oleg.gorbulich@sparkybit.com>
2023-03-05 19:43:04 +0200

    [29] Modal Window. Portal.

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,9 +1,11 @@
 import { Story } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
 
 export const ThemeDecorator = (theme:Theme) => (story: ()=> Story) => (
-    <div className={`app ${theme}`}>
-        {story()}
-    </div>
+    <ThemeProvider initialTheme={theme}>
+        <div className={`app ${theme}`}>
+            {story()}
+        </div>
+    </ThemeProvider>
 );
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'helpers/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useCallback, useState } from 'react';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 import cls from './Navbar.module.scss';
 
@@ -8,10 +9,29 @@ interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            /
+export const Navbar = ({ className }: NavbarProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsOpen((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                Enter
+            </Button>
+            <Modal isOpen={isOpen} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Sunt blanditiis aliquam,
+                a molestiae quis architecto? Veritatis placeat
+                explicabo eos maxime aliquam quos ipsum, quibusdam
+                alias nulla facilis culpa ab accusamus!
+            </Modal>
         </div>
-    </div>
-);
+    );
+};
